b90297bf71396bf90b47252773d058a26c6e0b0c
Paulo60650 <58972637+Paulo60650@users.noreply.github.com>
2020-01-30 16:53:51 +0100

    first commit

---
+++ exercice1.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+  <?php 
+  for ($i = 1; $i <= 149; $i += 2) {
+    echo $i."<br>";
+  }
+  ?> 
+</body>
+</html>
---
+++ exercice2.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+  <?php 
+  for ($i = 1; $i <= 500; $i++) { 
+    echo "Je dois faire des sauvegardes régulières de mes fichiers."."<br>";  
+  } 
+  ?> 
+</body>
+</html>
